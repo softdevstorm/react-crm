ce7ef94c621c451ec18f309ced3edd6dde870c87
softdevstorm <softdevstorm@gmail.com>
2020-05-07 15:13:38 +0200

    config

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 export default {
-    // apiUrl: "https://api.crmspy.com",
-    apiUrl: "http://192.168.0.215:5000",
+    apiUrl: "https://api.crmspy.com",
     storageUrl: "https://storage.googleapis.com/crmspy/uploads",
     assetsUrl: "https://storage.googleapis.com/api_v1/assets/",
     htmlUrl: "https://storage.googleapis.com/api_v1/html/"
